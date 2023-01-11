38b70708b8005b72cefe240111dd546c257d5fbc
ExequielPina <exequiel_pina@hotmail.es>
2023-01-11 12:18:50 +0100

    commit 1.4 creando variable de entorno para la URL

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,4 +1,9 @@
-
+export async function loader() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+  const resultado = await respuesta.json()
+  console.log(resultado)
+  return {}
+}
 
 function Tienda() {
   return (
